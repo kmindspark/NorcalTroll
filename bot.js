108d9f900539b953368cd84da3a1c8a59b12c455
kmindspark <19kaushiks@gmail.com>
2018-11-18 13:36:42 -0800

    Revert

--- bot.js
+++ bot.js
@@ -9,15 +9,6 @@ client.on('message', message => {
    if (message.content === 'ping') {
       message.reply('pong');
    }
-   if (message.content.contains("winning worlds")) {
-      message.channel.send("8k WINNING WORLDS!")
-   }
-   if (message.content.startsWith("!predict")) {
-      message.channel.send("")
-   }
-   if (message.content.endsWith(".")) {
-      message.delete(2000);
-   }
 });
 
 // THIS  MUST  BE  THIS  WAY
