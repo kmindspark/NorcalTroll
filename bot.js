9c6895a5f097f2662b9b9d510f4ea8e6c342abce
kmindspark <19kaushiks@gmail.com>
2018-11-18 14:52:10 -0800

    Add new feature

--- bot.js
+++ bot.js
@@ -1,6 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+function sleep(milliseconds) {
+   var start = new Date().getTime();
+   for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds) {
+         break;
+      }
+   }
+}
+
 client.on('ready', () => {
    console.log('I am ready!');
 });
@@ -19,6 +28,14 @@ client.on('message', message => {
    }
    if (message.content.toLowerCase().includes("leek")) {
       message.channel.send('fingerslip?');
+      sleep(1000);
+      message.channel.send('3');
+      sleep(1000);
+      message.channel.send('2');
+      sleep(1000);
+      message.channel.send('1');
+      sleep(1000);
+      message.channel.send('made u look');
    }
    if (message.content.toLowerCase().includes("vex is bad")) {
       message.channel.send('that\'s why I quit');
