2916491d5b8a3d72fa7ca84925e93f7767b077cb
kmindspark <19kaushiks@gmail.com>
2018-11-18 13:51:38 -0800

    Add more features

--- bot.js
+++ bot.js
@@ -9,11 +9,20 @@ client.on('message', message => {
    if (message.content === 'ping') {
       message.reply('pong.');
    }
-   if (message.content.toLowerCase().includes('winning worlds')) {
+   else if (message.content.toLowerCase().includes('winning worlds')) {
       message.channel.send('8k IS WINNING WORLDS!')
    }
-   if (message.content.endsWith(".")) {
+   else if (message.content.endsWith(".")) {
       message.delete(1000);
+   }
+   else if (message.content.toLowerCase().includes("leek")) {
+      message.channel.send('fingerslip?');
+   }
+   else if (message.content.toLowerCase().includes("vex is bad")) {
+      message.channel.send('that\'s why I quit');
+   }
+   else if ((Math.floor((Math.random() * 50) + 1)) == 42) {
+      message.channel.send('Hello everyone!')
    }
 });
 
