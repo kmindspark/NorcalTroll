b086d0f3db63e6bcf7e5bcd6e365010db8cda0b2
kmindspark <19kaushiks@gmail.com>
2018-11-23 17:14:17 -0800

    more test

--- bot.js
+++ bot.js
@@ -83,8 +83,7 @@ client.on('message', message => {
          message.reply("Feature in progress")
 
          let resp = httpGet("https://api.vexdb.io/v1/get_matches?team=" + teams[2]);
-         message.reply(resp);
-         /*let parsedResp = JSON.parse(resp);
+         let parsedResp = JSON.parse(resp);
          if (teams.length == 3) {
             message.reply(parsedResp[0]);
          }
