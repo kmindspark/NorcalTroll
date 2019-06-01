03277f98b45343ec33386779dfae88a6e557a61e
kmindspark <19kaushiks@gmail.com>
2019-06-01 10:05:20 -0700

    test

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ let cumulativeNames = ""
 
 function addNicknameIfValid(key) {
    message.reply(key.displayName)
-   let nickname = key.displayName;
+   /*let nickname = key.displayName;
    if (nickname.includes(teams[2].toUpperCase())) {
       cumulativeNames += nickname + "\n"
-   }
+   }*/
 }
 
 function httpGet(theUrl) {
